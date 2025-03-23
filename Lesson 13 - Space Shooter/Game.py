43290395b37e9dad8a27faf491fcfcc339ea510d
Aryan160512 <157271425+Aryan160512@users.noreply.github.com>
2025-03-23 10:59:58 +0000

    Add files via upload

--- Lesson 13 - Space Shooter/Game.py
+++ Lesson 13 - Space Shooter/Game.py
@@ -5,6 +5,9 @@ HEIGHT = 600
 TITLE = 'Space Shooter'
 
 isGameOver = False
+score = 0
+
+message = ''
 
 playerBulletSpeed = 6
 enemyBulletSpeed = 6
@@ -16,11 +19,14 @@ shooter.pos = (500, 550)
 
 enemies = []
 bullets = []
+enemyBullets = []
+
+level = 1
 
 def createEnemy():
     rand = random.randint(1, 3)
     enemy = Actor(f'enemy{rand}')
-    enemy.pos = (random.randint(50, 950), 50)
+    enemy.pos = (random.randint(50, 950), 0)
     enemies.append(enemy)
 
 def playerBullets():
@@ -28,7 +34,32 @@ def playerBullets():
     bullet.pos = (shooter.x, shooter.y + 20)
     bullets.append(bullet)
 
+def checkCollison():
+    global score
+    for bullet in bullets:
+        for enemy in enemies:
+            if bullet.colliderect(enemy):
+                bullets.remove(bullet)
+                enemies.remove(enemy)
+                score += 1
+
+def on_key_down(key):
+    if key == keys.SPACE:
+        playerBullets()
+
+def nextLevel():
+    global enemies, bullets
+    enemies = []
+    bullets = []
+
+def createEnemyBullets(alien):
+    enemyBullet = Actor('enemyBullet')
+    enemyBullet.pos = (alien.x, alien.y + 20) 
+    enemyBullets.append(enemyBullet)   
+
+
 def update():
+    global isGameOver, message, level
     if random.random() < 0.01:
         createEnemy()
 
@@ -37,8 +68,8 @@ def update():
     elif keyboard.right and shooter.x < 950:
         shooter.x += 5
 
-    if keyboard.space:
-        playerBullets()
+    # if keyboard.space:
+    #     playerBullets()
 
     for bullet in bullets:
         bullet.y -= playerBulletSpeed
@@ -48,21 +79,50 @@ def update():
 
     for enemy in enemies:
         enemy.y += random.randint(1, 5)
-        
-        if enemy.y > 1000:
+        if enemy.colliderect(shooter):
             isGameOver = True
-            gameOver('YOU LOST')
+            message = 'YOU LOSE'
+        
+        if enemy.y > 650:
+            isGameOver = True
+            message = 'YOU LOSE'
 
-def gameOver(message):
-    screen.draw.text(message, (500, 300))
+    if score == 3:
+        nextLevel()
+        level = 2
+
+    for enemyBullet in enemyBullets:
+        enemyBullet.y += enemyBulletSpeed
+
+        if enemyBullet.y > 1000:
+            enemyBullets.remove(enemyBullet)
+
+    if level == 2:
+        if random.random() < 0.01:
+            createEnemy()
+        
+        for enemy in enemies:
+            if random.random() < 0.01:
+                createEnemyBullets(enemy)
+                
 
 def draw():
     screen.clear()
     screen.blit('background', (0, 0))
 
+    checkCollison()
+
+    screen.draw.text(f'Score: {score}', (50, 50))
+
+    if isGameOver:
+        screen.draw.text(message, (500, 300))
+
     if isGameOver == False:
         for bullet in bullets:
             bullet.draw()
+
+        for enemyBullet in enemyBullets:
+            enemyBullet.draw()
 
         shooter.draw()
 
